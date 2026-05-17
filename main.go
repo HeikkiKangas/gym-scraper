3d145f2eaff7270ef842a2b1e3be1a0418580bb5
Heikki Kangas <toxiici@gmail.com>
2026-05-17 09:33:11 +0300

    Refactor parser functions to own file

--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"fmt"
-	"net/mail"
 	"os"
 	"slices"
 	"strings"
 	"sync"
 	"time"
 
-	"github.com/PuerkitoBio/goquery"
 	"github.com/gocolly/colly"
 )
 
@@ -133,91 +131,4 @@ func main() {
 		fmt.Println("Failed to remove cache directory:", err)
 		os.Exit(1)
 	}
-}
-
-func isValidEmail(s string) bool {
-	if strings.Count(s, "@") != 1 {
-		return false
-	}
-
-	addr, err := mail.ParseAddress(s)
-	if err != nil {
-		return false
-	}
-
-	return addr.Address == s
-}
-
-func parseCitiesFromSelection(selection *goquery.Selection) []string {
-	cities := []string{}
-	selection.Children().Each(func(i int, s *goquery.Selection) {
-		value, ok := s.Attr("value")
-		if !ok || value == "-1" || strings.TrimSpace(value) == "" {
-			return
-		}
-		cities = append(cities, fmt.Sprintf("https://kuntosali.fi/kaupungit/%v/", value))
-	})
-	return cities
-}
-
-func parseGymURLFromElement(e *colly.HTMLElement) (string, bool) {
-	url := strings.TrimSpace(e.Attr("href"))
-	return url, url != ""
-}
-
-func parseEmailFromText(text string) (string, bool) {
-	trimmed := strings.TrimSpace(text)
-	addr, err := mail.ParseAddress(trimmed)
-	if err == nil {
-		trimmed = addr.Address
-	}
-
-	if !isValidEmail(trimmed) {
-		return "", false
-	}
-
-	return trimmed, true
-}
-
-func parseCitiesFromHTML(html string) ([]string, error) {
-	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
-	if err != nil {
-		return nil, err
-	}
-	selection := doc.Find("div.kaupunkilaatikko form#kaupunki-valinta select#kaupunki")
-	return parseCitiesFromSelection(selection), nil
-}
-
-func parseGymURLsFromHTML(html string) ([]string, error) {
-	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
-	if err != nil {
-		return nil, err
-	}
-
-	urls := []string{}
-	doc.Find("div.salilistaus-simple a.salin-nimi-kaupunki[href]").Each(func(i int, s *goquery.Selection) {
-		url := strings.TrimSpace(s.AttrOr("href", ""))
-		if url != "" {
-			urls = append(urls, url)
-		}
-	})
-
-	return urls, nil
-}
-
-func parseEmailsFromHTML(html string) ([]string, error) {
-	doc, err := goquery.NewDocumentFromReader(strings.NewReader(html))
-	if err != nil {
-		return nil, err
-	}
-
-	emails := []string{}
-	doc.Find("div.sali-data div#salin-info p").Each(func(i int, s *goquery.Selection) {
-		email, ok := parseEmailFromText(s.Text())
-		if ok && !slices.Contains(emails, email) {
-			emails = append(emails, email)
-		}
-	})
-
-	return emails, nil
 }
